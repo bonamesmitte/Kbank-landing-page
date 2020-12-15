a70e9715ecbb6b056447403d9acbedc3cdd2b2ac
Alice <innycity84@gmail.com>
2020-12-15 03:59:31 -0600

    applied media

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-{/* <home> */}
+function navToggle(){
+    const navToggle = document.querySelector('.navToggle');
+    const nav  = document.querySelector('.nav');
+    navToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+
